12071f8fd7591a90d48d0daeff363c534acc67a4
Arto Ruotsala <arto.ruotsala@gmail.com>
2022-09-29 10:04:30 +0300

    add telegram notifications

--- src/binance/sendOrderResponse.ts
+++ src/binance/sendOrderResponse.ts
@@ -1,21 +1,23 @@
 import { OrderResponseFull, OrderType } from 'binance'
 import { resetTickerAndStoreToDb } from '../db/resetTickerAndStoreToDb'
 import { storeOrderToDb } from '../db/storeOrderToDb'
+import { sendMessageToTelegram } from '../notifications/sendMessageToTelegram'
 
 export const sendOrderResponse = (
   orderStatus: OrderResponseFull,
   orderType: string
 ) => {
   if (orderStatus && orderStatus.status && orderStatus.status === 'FILLED') {
-    if (orderType.toUpperCase() === 'BUY')
+    if (orderType.toUpperCase() === 'BUY') {
       storeOrderToDb(orderStatus.symbol, orderStatus.executedQty as string)
-    else
+    } else
       resetTickerAndStoreToDb(
         orderStatus.symbol,
         orderStatus.executedQty as string,
         orderStatus.price as string
       )
 
+    sendMessageToTelegram(orderStatus)
     return { status: 'Order Placed', code: 200 }
   }
   return { status: 'Order Failed', code: 400 }
---
+++ src/notifications/sendMessageToTelegram.ts
@@ -0,0 +1,14 @@
+import { OrderResponseFull } from 'binance'
+import { TelegramBot } from 'telegram-bot-nodejs'
+
+const bot = new TelegramBot(
+  process.env.TELEGRAM_TOKEN_LIVE!,
+  process.env.TELEGRAM_CHAT_ID_LIVE!
+)
+
+export async function sendMessageToTelegram(orderStatus: OrderResponseFull) {
+  const buySell = orderStatus.side === 'BUY' ? '🟢' : '🔴'
+  await bot.sendMessage(
+    `Order Placed: ${buySell} ${orderStatus.symbol} ${orderStatus.executedQty} at ${orderStatus.price}`
+  )
+}
